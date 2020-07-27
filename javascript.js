6bb2b94d682209fa14df771e307c7dcb16cfbcd5
ONogueira <ti3atuu@gmail.com>
2020-07-27 10:56:57 -0300

    Arrumei a posição dos elements

--- javascript.js
+++ javascript.js
@@ -1,48 +1,46 @@
     
     
-    var actionButton = document.querySelector('input#action')
-    var selectButton = document.querySelector('select#operation')
+var actionButton = document.querySelector('input#action')
+var selectButton = document.querySelector('select#operation')
     
-    //Listener
-    actionButton.addEventListener('click',operationFunc)
+//Listener
+actionButton.addEventListener('click',operationFunc)
 
     //Operações
-    function operationFunc()
-    {
-        var num1 = Number(document.querySelector('input#input1').value)
-        var num2 = Number(document.querySelector('input#input2').value)
-        var result;
+function operationFunc()
+{
+    var num1 = Number(document.querySelector('input#input1').value)
+    var num2 = Number(document.querySelector('input#input2').value)
+    var result;
 
-        const operations = {
-            addition()
-            {
-            result = num1 + num2
-            },
-            subtraction()
-            {
-            result = num1 - num2;
-            }
-        }
-
-        switch(selectButton.value)  //Checar qual opcao esta selecionada
+    const operations = {
+        addition()
         {
-            case 'addition':
-                operations.addition()
-            break
-            
-            case 'subtraction':
-                operations.subtraction()
-            break
+            result = num1 + num2
+        },
+        subtraction()
+        {
+            result = num1 - num2;
         }
-         
-        
-
-        document.querySelector('div#result').innerHTML = `Result: ${result}`
-        console.log(`A operação ${selectButton.value} entre ${num1} e ${num2} teve um resultado de  ${result}`)
     }
 
-    function updatePage()
+    switch(selectButton.value)  //Checar qual opcao esta selecionada
     {
-        var h1 = document.querySelector('h1#tittle')
-        h1.innerHTML = selectButton.value[0].toUpperCase() + selectButton.value.slice(1)
+        case 'addition':
+            operations.addition()
+        break
+            
+        case 'subtraction':
+            operations.subtraction()
+        break
     }
+
+    document.querySelector('div#result').innerHTML = `Result: ${result}`
+    console.log(`A operação ${selectButton.value} entre ${num1} e ${num2} teve um resultado de  ${result}`)
+    }
+
+function updatePage()
+{
+    var h1 = document.querySelector('h1#tittle')
+    h1.innerHTML = selectButton.value[0].toUpperCase() + selectButton.value.slice(1)
+}
